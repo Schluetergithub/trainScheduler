f40c52f0a2ffbf5b2d61729be35d1d73a11d6e11
Schlueter <SchlueterPro@gmail.com>
2018-08-01 14:35:50 -0700

    finished the moment js stuff. fixed the issue I had pushing the wrong train time to the table. el fin.

--- assets/train.js
+++ assets/train.js
@@ -1,4 +1,5 @@
-// Initialize Firebase
+
+                // Initialize Firebase
 var config = {
     apiKey: "AIzaSyBSVlImept_z14nYK-KSSyhEOKj5_GIPC8",
     authDomain: "myfirstdbproject-58c81.firebaseapp.com",
@@ -7,15 +8,15 @@ var config = {
     storageBucket: "myfirstdbproject-58c81.appspot.com",
     messagingSenderId: "48807571804"
   };
+  
   firebase.initializeApp(config);
 
   var database = firebase.database();
 
 
-
+  $(document).ready(function () {
 
   $("#submit-button").on("click", function() {
-
       database.ref().push( {
 
       trainName: $("#train-name").val().trim(),
@@ -23,100 +24,81 @@ var config = {
       trainTime: $("#train-time").val().trim(),
       trainFrequency: $("#frequency").val().trim()
 
+
       });
 
     });
 
 
-    database.ref().on("child_added", function (snapshot){
-        var Name = snapshot.val().trainName;
+    database.ref().on("child_added", function (snapshot) {
 
-        var Destination = snapshot.val().trainDestination;
+        var name = snapshot.val().trainName;
 
-        var Time = snapshot.val().trainTime;
+        var destination = snapshot.val().trainDestination;
 
-        var Frequency = snapshot.val().trainFrequency;
+        var time = snapshot.val().trainTime;
+
+        var frequency = snapshot.val().trainFrequency;
+
+        var tFrequency = frequency;
+
+        // Time is 3:30 AM
+        var firstTime = time;
+
+        // First Time (pushed back 1 year to make sure it comes before current time)
+        var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+        console.log(firstTimeConverted);
+
+        // Current Time
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+        // Difference between the times
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+
+        // Time apart (remainder)
+        var tRemainder = diffTime % tFrequency;
+        console.log(tRemainder);
+
+        // Minute Until Train
+        var tMinutesTillTrain = tFrequency - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+        // Next Train
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+
+
+
+        // var minutesAway = tMinutesTillTrain;
 
         var newRow = $("<tr>").append(
-            $("<td>").text(Name),
-            $("<td>").text(Destination),
-            $("<td>").text(Time),
-            $("<td>").text(Frequency),
+            $("<td>").text(name),
+            $("<td>").text(destination),
+            // $("<td>").text(time),
+            $("<td>").text(nextTrain),
+            $("<td>").text(frequency),
+            $("<td>").text(tMinutesTillTrain),
 
         );
 
-        $("table-body").append(newRow);
+
+        
+    
+
+        $(".tbody").append(newRow);
 
     }, function (errorObject) {
         console.log("the read failed " + errorObject.code)
     }
 
+    
+
 );
 
-    
-
-    
 
 
-// Assume the following situations.
 
-                // (TEST 1)
-                // First Train of the Day is 3:00 AM
-                // Assume Train comes every 3 minutes.
-                // Assume the current time is 3:16 AM....
-                // What time would the next train be...? (Use your brain first)
-                // It would be 3:18 -- 2 minutes away
-
-                // (TEST 2)
-                // First Train of the Day is 3:00 AM
-                // Assume Train comes every 7 minutes.
-                // Assume the current time is 3:16 AM....
-                // What time would the next train be...? (Use your brain first)
-                // It would be 3:21 -- 5 minutes away
-
-
-                // ==========================================================
-
-                // Solved Mathematically
-                // Test case 1:
-                // 16 - 00 = 16
-                // 16 % 3 = 1 (Modulus is the remainder)
-                // 3 - 1 = 2 minutes away
-                // 2 + 3:16 = 3:18
-
-                // Solved Mathematically
-                // Test case 2:
-                // 16 - 00 = 16
-                // 16 % 7 = 2 (Modulus is the remainder)
-                // 7 - 2 = 5 minutes away
-                // 5 + 3:16 = 3:21
-
-                // Assumptions
-                var tFrequency = 3;
-
-                // Time is 3:30 AM
-                var firstTime = "03:30";
-
-                // First Time (pushed back 1 year to make sure it comes before current time)
-                var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-                console.log(firstTimeConverted);
-
-                // Current Time
-                var currentTime = moment();
-                console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-                // Difference between the times
-                var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-                console.log("DIFFERENCE IN TIME: " + diffTime);
-
-                // Time apart (remainder)
-                var tRemainder = diffTime % tFrequency;
-                console.log(tRemainder);
-
-                // Minute Until Train
-                var tMinutesTillTrain = tFrequency - tRemainder;
-                console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-                // Next Train
-                var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-                console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+});
